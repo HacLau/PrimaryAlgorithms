141847f44c47d043f327fcca8bf0e62d9e24f2af
liushunlin <liushunlin940720@126.com>
2019-09-09 22:26:28 +0800

    commit the first Uniq Char code and finish result

--- src/com/lsl/string/FirstUniqChar.java
+++ src/com/lsl/string/FirstUniqChar.java
@@ -1,25 +1,31 @@
 package com.lsl.string;
 
 public class FirstUniqChar {
-
+	/**
+	 * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+	 * 案例:
+	 * s = "leetcode"
+	 * 返回 0.
+	 * s = "loveleetcode",
+	 * 返回 2.
+	 * 注意事项：您可以假定该字符串只包含小写字母。
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String string = "ccl";
+		String string = "cc";
 		System.out.println(firstUniqChar(string));
 	}
 	public static int firstUniqChar(String s) {
-		boolean b = false;
+		
 		int index = -1;
 		for (int i = 0; i < s.length(); i++) {
-			b = false; 
 			for (int j = 0; j < s.length(); j++) {
-				if(i != j) {
-					if (s.charAt(i) == s.charAt(j)) {
-						b = true;
-					}
-					if (j == s.length() - 1 && !b) {
-						return i;
-					}
+				if(i != j && s.charAt(i) == s.charAt(j)) {
+					break;
+				}
+				if(j == s.length() - 1) {
+					return i;
 				}
 			}
 		}
